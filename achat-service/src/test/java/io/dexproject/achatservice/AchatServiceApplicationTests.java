d381ad2a728803a5f4c8686a783049e889bbe877
DexDevNL2023 <dexternlang@gmail.com>
2023-12-20 08:21:27 +0100

    config okay

--- achat-service/src/test/java/io/dexproject/achatservice/AchatServiceApplicationTests.java
+++ achat-service/src/test/java/io/dexproject/achatservice/AchatServiceApplicationTests.java
@@ -1,7 +1,6 @@
 package io.dexproject.achatservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AchatServiceApplicationTests {
