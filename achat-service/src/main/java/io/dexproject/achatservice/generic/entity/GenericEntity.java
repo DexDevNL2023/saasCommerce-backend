3944f8f7805da0df466ce00d2db084bc839c5f32
DexDevNL2023 <dexternlang@gmail.com>
2024-01-30 10:03:33 +0100

    update 50

--- achat-service/src/main/java/io/dexproject/achatservice/generic/entity/GenericEntity.java
+++ achat-service/src/main/java/io/dexproject/achatservice/generic/entity/GenericEntity.java
@@ -3,7 +3,6 @@ package io.dexproject.achatservice.generic.entity;
 public interface GenericEntity<E> {
     // met à jour l'instance actuelle avec les données fournies
     void update(E source);
-
     boolean equalsToDto(E source);
     void setId(Long id);
     Long getId();
